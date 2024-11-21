a669f7b7fce27d59dfea4c54c4ca9425e05efb9a
shogo_koide <shogo10261026@gmail.com>
2024-11-22 08:44:54 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  root: "src",
+  plugins: [react()],
+  publicDir: resolve(__dirname, "public"),
+  build: {
+    // distフォルダに出力
+    outDir: resolve(__dirname, "dist"),
+    // 存在しないときはフォルダを作成する
+    emptyOutDir: true,
+    copyPublicDir: true,
+    rollupOptions: {
+      // entry pointがあるindex.htmlのパス
+      input: {
+        "": resolve(__dirname, "src/index.html"),
+      },
+      // bundle.jsを差し替えする
+      output: {
+        entryFileNames: "assets/bundle.js",
+      },
+    },
+  },
+});
