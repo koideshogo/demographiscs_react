e566ed2ade413ac51552a53d63347b86bc4d84f9
shogo_koide <shogo10261026@gmail.com>
2024-11-27 01:30:30 +0900

    feat: セレクトボックスの生成方法を変更

--- src/hooks/useSelectTypePopulation.tsx
+++ src/hooks/useSelectTypePopulation.tsx
@@ -1,28 +1,29 @@
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import {SelectTypePopulationProps} from "../_types/population";
 
-const TYPES: string[] = [
-    '総人口',
-    '年少人口',
-    '生産年齢人口',
-    '老年人口'
+const GRAPH_TYPES= [
+    { "value" : '0', "label" : '総人口'},
+    { "value" : '1', "label" : '年少人口'},
+    { "value" : '2', "label" : '生産年齢人口'},
+    { "value" : '3', "label" : '老年人口'},
 ];
 
 const useSelectTypePopulation = (): SelectTypePopulationProps => {
-    const [populationType, setPopulationType] = useState<string>("総人口");
+    const [graphType, setGraphType] = useState('0');
 
     function changePopulationType(e: ChangeEvent<HTMLSelectElement>): void {
         const { value } = e.target;
 
-        if (TYPES.includes(value)) {
-            setPopulationType(value);
+        const collectTypeValue = GRAPH_TYPES.map((type) => type.value);
+        if (collectTypeValue.includes(value)) {
+            setGraphType(value);
         }
     }
 
     return {
-        populationType: populationType,
         handleChange: changePopulationType,
-        options: TYPES
+        options: GRAPH_TYPES,
+        checkedValue: graphType
     }
 }
 
