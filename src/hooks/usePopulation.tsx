857b41d4a4b78a3631a34e71bdc8b4f6e2e67f14
shogo_koide <shogo10261026@gmail.com>
2024-11-27 04:20:16 +0900

    feat: グラフデータの取得処理を実装

--- src/hooks/usePopulation.tsx
+++ src/hooks/usePopulation.tsx
@@ -1,17 +1,18 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {getPopulation} from "../api/population";
 
 const usePopulation = (
-    populationType: string
+    SelectedGraphType: string
 ) => {
     const [populationData, setPopulationData] = useState({
         boundaryYear: 0,
-        total: [],
-        young: [],
-        workingAge: [],
-        elderly: [],
+        prefectureCode: 0,
+        prefectureName: '',
+        total: {label: '', data: [{'year' : 0, 'value': 0, 'rate': 0}]},
+        young: {label: '', data: [{'year' : 0, 'value': 0, 'rate': 0}]},
+        workingAge: {label: '', data: [{'year' : 0, 'value': 0, 'rate': 0}]},
+        elderly: {label: '', data: [{'year' : 0, 'value': 0, 'rate': 0}]},
     });
-
     const [checkedPrefectures, setCheckedPrefecture] = useState([]);
     const updatePopulationData = useCallback(async (checkPrefectures: []) => {
         setCheckedPrefecture(checkPrefectures);
@@ -19,13 +20,30 @@ const usePopulation = (
 
     const fetchAllPopulationData = async () => {
         try {
-            return await Promise.all(checkedPrefectures.map((prefecture) => getPopulation(prefecture.id)));
+            return await Promise.all(checkedPrefectures.map((prefecture) => getPopulation(prefecture.id, prefecture.name)));
         } catch (error) {
             console.error('Error fetching population data:', error);
         }
     };
     // データ取得
-    const data = fetchAllPopulationData();
+    useEffect(() => {
+        fetchAllPopulationData().then((data) => {
+            const populationByGraph = data.map((population) => {
+                const populationResultData = population.result.data;
+                return {
+                    prefectureName: population.prefectureName,
+                    prefectureCode: population.prefectureCode,
+                    boundaryYear: population.result.boundaryYear,
+                    total: populationResultData[0],
+                    young: populationResultData[1],
+                    workingAge: populationResultData[2],
+                    elderly: populationResultData[3],
+                };
+            });
+
+            setPopulationData(populationByGraph);
+        });
+    }, [checkedPrefectures, SelectedGraphType]);
 
     return { populationData, updatePopulationData };
 }
