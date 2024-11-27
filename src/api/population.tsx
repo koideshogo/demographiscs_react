a6eb1709e7d2c1778b60cc5d8323e3a2a211ee18
shogo_koide <shogo10261026@gmail.com>
2024-11-27 15:38:17 +0900

    feat: 人口統計の API のレスポンスの中に都道府県情報を付与

--- src/api/population.tsx
+++ src/api/population.tsx
@@ -4,16 +4,20 @@ import {PopulationResponse} from "../_types/api/population";
 /**
  * 都道府県別の人口データを取得する
  */
-export const getPopulation = async (prefCode: string): Promise<PopulationResponse> => {
+export const getPopulation = async (prefectureCode: string, prefectureName: string): Promise<PopulationResponse> => {
     try {
         const response = await axiosV1Instance.get('/population/composition/perYear', {
             params: {
-                prefCode: prefCode,
+                prefCode: prefectureCode,
             },
         });
-        console.log('ok')
-        console.log(response.data.result);
-        return response.data.result;
+
+        return {
+            prefectureCode: prefectureCode,
+            prefectureName: prefectureName,
+            message: response.data.message,
+            result: response.data.result
+        };
     } catch (error) {
         console.error(error);
         throw new Error('Failed to get population');
