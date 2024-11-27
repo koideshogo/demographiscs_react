ccb152f94920efd8eb8df1c39bdf486c7179db39
shogo_koide <shogo10261026@gmail.com>
2024-11-27 15:35:27 +0900

    feat: グラフの表示を行えるようにデータの調整

--- src/components/organisms/population-graphs.tsx
+++ src/components/organisms/population-graphs.tsx
@@ -1,20 +1,50 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import React, {useRef} from 'react';
+import {allGeneratedPositionsFor} from "@jridgewell/trace-mapping";
 
 type Props = {
     populationList: populationListType
 }
 
 const PopulationGraph: React.FC<Props> = ({ populationList }) => {
+    if (populationList.length === 0) {
+        return <></>;
+    }
+
+    const years  = [ ...new Set(populationList.flatMap((data) => data.result.data.map((item) => item.year)))];
+
+    console.log(populationList)
+    console.log(years)
+    const options: Highcharts.Options = {
+        title: undefined,
+        xAxis: {
+            title: {
+                text: '年',
+            },
+            categories: years,
+        },
+        yAxis: {
+            title: {
+                text: '人口',
+            },
+        },
+        series: populationList.map((data) => ({
+            type: 'line',
+            name: data.prefectureName,
+            data: data.result.data.map((item) => item.value)
+        })),
+        accessibility: {
+            enabled: false,
+        },
+    };
 
     return (
-        <HighchartsReact
-            highcharts={HighCharts}
-            options={options}
-            ref={chartComponentRef}
-        />
-    )
+        <section>
+            <div>
+                <HighchartsReact highcharts={Highcharts} options={options} />
+            </div>
+        </section>
+    );
 }
 
 export default PopulationGraph;
