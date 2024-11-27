22024471d9f960c639101c59b2c992a083220d9d
shogo_koide <shogo10261026@gmail.com>
2024-11-27 15:37:07 +0900

    feat: ダッシュボード画面の実装

--- src/components/templates/dashboard.tsx
+++ src/components/templates/dashboard.tsx
@@ -7,9 +7,7 @@ import useSelectTypePopulation from "../../hooks/useSelectTypePopulation";
 
 export const Dashboard = () => {
     const selectBoxProps = useSelectTypePopulation();
-    const { populationGraph, updatePopulationData } = usePopulation(
-        selectBoxProps.populationType
-    );
+    const { populationData, updatePopulationData } = usePopulation(selectBoxProps.selectedValue);
     const { prefectureCheckBoxInfo, isLoading } = usePrefectureCheckBox(updatePopulationData);
     if (isLoading) {
         return <p>Loading data, please wait...</p>;
@@ -17,13 +15,22 @@ export const Dashboard = () => {
 
     return (
         <>
-            <div>
-                <h1>人口統計グラフ</h1>
-                <SelectBox
-                    props={selectBoxProps}
-                />
-                <PrefectureCheckBox prefectureCheckBoxInfo={prefectureCheckBoxInfo} />
-                {/*<PopulationGraph populationList={populationGraph} />*/}
+            <div className="min-h-screen bg-black-50 flex flex-col justify-center items-center p-6">
+                <div className="w-full max-w-6xl bg-black shadow-lg rounded-lg p-8 space-y-8">
+                    <h1 className="text-3xl font-bold text-white-800 text-center">人口統計グラフ</h1>
+
+                    <div className="flex justify-center">
+                        <SelectBox props={selectBoxProps} />
+                    </div>
+
+                    <div className="flex justify-center">
+                        <PrefectureCheckBox prefectureCheckBoxInfo={prefectureCheckBoxInfo} />
+                    </div>
+
+                    <div className="flex justify-center">
+                        <PopulationGraph populationList={populationData} selectedGraphType />
+                    </div>
+                </div>
             </div>
         </>
     );
