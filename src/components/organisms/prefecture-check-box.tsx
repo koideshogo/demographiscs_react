0c2dead165d0f63f496fa072b6bc9b595572fc19
shogo_koide <shogo10261026@gmail.com>
2024-11-27 15:38:52 +0900

    feat: check box レイアウトを調整

--- src/components/organisms/prefecture-check-box.tsx
+++ src/components/organisms/prefecture-check-box.tsx
@@ -1,16 +1,25 @@
-import Index from "../atoms/checkBox";
+import CheckBox from "../atoms/checkBox";
 import Label from "../atoms/label";
 import React from "react";
 
 const PrefectureCheckBox: React.FC<Props> = ({ prefectureCheckBoxInfo }) => {
     return (
         <>
-            {prefectureCheckBoxInfo.map((info) => (
-                <div key={info.id}>
-                    <Label htmlFor={info.id} label={info.label} />
-                    <Index key={info.id} {...info} />
+            <div className="bg-black-50 flex justify-center items-center p-4">
+                <div className="w-full bg-black shadow-lg rounded-lg p-6">
+                    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+                        {prefectureCheckBoxInfo.map((info) => (
+                            <div
+                                key={info.id}
+                                className="flex items-center justify-between p-4 border border-gray-300 rounded-lg"
+                            >
+                                <Label htmlFor={info.id} label={info.label} />
+                                <CheckBox {...info} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            ))}
+            </div>
         </>
     );
 }
